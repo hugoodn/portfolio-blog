e12f2c977b7e040b4c5c136a0f71a993cc77a0d3
Hugo Donné <hugoodn@Hugos-MacBook-Air.local>
2024-10-01 01:18:58 +0200

    a propos

--- burger_menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const burgerMenu = document.getElementById("burger-menu");
-    const navLinks = document.getElementById("nav-links");
-
-    burgerMenu.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-    });
-});
